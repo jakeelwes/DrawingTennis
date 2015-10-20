f83f011f1e54ed01cc4863ba7e7ec403203c5965
Jake Elwes <jakeelwes@2907.artic.edu>
2015-10-20 13:07:54 -0500

    ajax attempt commented out and save button

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ window.onload = function() {
   var gui = new dat.GUI();
   gui.add(window, 'BrushSize', 0, 100);// or dropdown - { Small: 5, Medium: 40, Large: 100 } );
   gui.add(window, 'Transparency', 0, 100);// { "10%": 25, "50%": 127, "100%": 255 } );
+  gui.add(window, 'Done');
 };
 
 // - - - - - - - - - - -
 
 var canvasSize;
-var BrushSize = 10;
+var BrushSize = 20;
 var Transparency = 100;
+var save = false;
+// var strokes[];
+var Done = function() {
+  save = true;
+}
 
 function setup(){
 
@@ -29,13 +35,36 @@ function setup(){
 
 function draw(){
   var alpha = map(Transparency, 0, 100, 0, 255);
-  var bsize = map(BrushSize, 0, 100, 0, canvasSize/3);
+  var bsize = map(pow(BrushSize,2), 0, 10000, 0, canvasSize/3);
+
+  noFill();
   stroke(0, alpha);
   strokeWeight(bsize);
+
   if (mouseIsPressed){
     line(pmouseX,pmouseY,mouseX,mouseY); //could use vertex and shape? - contant alpha
   }
 }
+
+
+
+// --------------- use ajax & jquery to write file to server?
+// $.ajax({
+//   type: 'POST',
+//   // url: url,//url of receiver file on server
+//   // data: data, //your data
+//   // success: success, //callback when ajax request finishes
+//   // dataType: dataType //text/json...
+// });
+
+// ---------------using shape
+// beginShape();
+// while (mouseIsPressed){
+//   vertex(mouseX, mouseY);
+//   vertex(0,0);
+// }
+// endShape();
+
 
 // ---------------resize - won't reload
 // function windowResized() {
